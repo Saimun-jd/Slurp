a934ca1a9f4a37b69f20899332cb8ec6c216e743
Saimun-jd <saimuncoder@gmail.com>
2021-05-29 19:04:01 +0600

    fix

--- Slurp/src/Slurp/Application.cpp
+++ Slurp/src/Slurp/Application.cpp
@@ -29,7 +29,12 @@ namespace Slurp {
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 
-		SL_CORE_TRACE("{0}", e);
+		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+		{
+			(*--it)->OnEvent(e);
+			if (e.Handled)
+				break;
+		}
 	}
 
 	void Application::PushLayer(Layer* layer)
@@ -48,6 +53,10 @@ namespace Slurp {
 		{
 			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			for (Layer* layer : m_LayerStack)
+				layer->OnUpdate();
+
 			m_Window->OnUpdate();
 		}
 	}
